bcca14d94cfaea289fa8311372a595d9deaf090d
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-03-09 18:52:25 -0300

    feat(components/post): Create post component

---
+++ src/components/Post.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Comment from "./Comment";
+
+function Post() {
+  return (
+    <>
+      post works
+      <Comment />
+    </>
+  );
+}
+
+export default Post;
