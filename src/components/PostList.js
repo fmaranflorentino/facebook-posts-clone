46e67acef9edd7589203304800565190e7e12a2f
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-03-09 18:44:14 -0300

    feat(PostList): Create PostList component

---
+++ src/components/PostList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PostList extends Component {
+  render() {
+    return (
+      <h1>Post List Works!</h1>
+    )
+  }
+}
+
+export default PostList;
